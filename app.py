53f460f64dd06a05f55a5bd94cfde31266f29f74
King_Crimson004 <k104.umarudabezt@gmail.com>
2024-11-13 17:09:34 +0700

    Track large files using Git LFS

---
+++ app.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:4adb3624c5588ff17747cb71c1256e3378acfea63c0c92ef7f9779c871eb0a21
+size 5530
---
+++ static/js/main.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:8e7bfd4d4497ce3cb2388b700893791b055539c5e4de8ebcf6ec411d9343b354
+size 1347
